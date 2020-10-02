ce785282508c18a2e0213e9377ec7c807dc2bff4
Elena <elena.ormaza.front.dp@gmail.com>
2020-10-02 16:50:46 +0200

    arreglos ok

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,13 +1,4 @@
 
-
-$('.barra-nivel').each(function() {
-	var valorLargura = $(this).data('nivel');
-	var valorNivel = $(this).html("<span class='valor-nivel'>"+valorLargura+"</span>");
-	  $(this).animate({
-		  width: valorLargura
-	  });
-});
-
 (function($) {
 
 	/**
